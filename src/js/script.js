b4b54819bf3bc941f6fcc6cf1cecf16c17c5072b
peppegiu <peppegiu2006@gmail.com>
2025-02-10 21:48:16 -0300

    Create a html range button that change the number of columns and rows
    The button makes the user customize the size of their grid.  Huge mistake: mixing event listeners from range to onclick because it will confuse the order that will be executed asides from affecting the perfomance

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,52 @@
-let grid = 16;
-let number_blocks = 16 ** 2;
 const containerEl = document.querySelector(".container");
+const label = document.querySelector(".range");
+const range = document.querySelector("#area");
+ 
+let grid;
+let number_blocks;
+let div;
+
+label.textContent = range.value
+range.addEventListener("input", () => {
+    label.textContent = range.value + "X" + range.value;
+    grid = Number(range.value);
+    number_blocks = grid ** 2;
+})
+
+function updateGrid() {
+    if (Number(containerEl.childElementCount == 0)) {
+        createDiv();
+        
+    }
+    
+    else {
+        containerEl.innerHTML = " ";
+        createDiv()
+    }
+        
+    
+}
+
+
 
 
 function createDiv() {
-    
-    containerEl.style.width = "300px"
+    containerEl.style.width = "960px"
     let containerWidth = containerEl.style.width;
     let new_width_var = containerWidth.replace("px", "");
     const div_size = new_width_var / grid;
     for (let i = 1; i <= number_blocks; i++) {
-        let div = document.createElement("div");
+        div = document.createElement("div");
         div.style.width = div_size + "px";
         div.style.height = div_size + "px";
         div.style.border = "1px solid gray";
         div.className = "box";
-        if (div) {
-            console.log("exists");
-        } else {
-            console.log('not loaded');
-        }
+        div.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "gray";
+        })
         
         containerEl.appendChild(div);
     }
 }
 
-createDiv();
-const containerItems = document.querySelectorAll(".box");
 
-containerItems.forEach((div) => {
-    div.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = "gray";
-    })
-})
